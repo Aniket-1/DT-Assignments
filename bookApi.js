01d04cbf74c444c70bd0bb8e134239ecdbb1560a
Aniket-1 <Aniket180101@gmail.com>
2021-09-17 10:21:04 +0530

    added protected routes

--- bookApi.js
+++ bookApi.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const booksData = require('./booksData.json')
 
+const url = 'localhost:3000/books'
 router.get('/', (req, res)=>{
     console.log("Getting All Books");
     res.json(booksData)
@@ -9,8 +10,16 @@ router.get('/', (req, res)=>{
 
 router.post('/', (req, res) => {
     console.log("Adding A Book!");
-    booksData.push(req.body)
-    res.json({message:"Book Added!"})
+    let { token } = url.parse(req.url, true).query.token;
+    if (process.env.token == token){
+        booksData.push(req.body)
+        res.json({message:"Book Added!"})
+    }
+    else{
+        res.json({message: "You're not an admin"})
+    }
+    
+    
 })
 
 router.get('/:id', (req, res) => {
@@ -23,22 +32,36 @@ router.put('/:id', (req, res) => {
     console.log("Updating A Book")
     const book = req.body
     const { id } = req.params
-    booksData.forEach((data, idx) => {
-        if (data.id === parseInt(id)){
-            booksData[idx] = book
-        }
-    });
-    res.json({message:"Book Updated!"})
+    let { token } = url.parse(req.url, true).query.token;
+    if (process.env.token == token){
+        booksData.forEach((data, idx) => {
+            if (data.id === parseInt(id)){
+                booksData[idx] = book
+            }
+        });
+        res.json({message:"Book Updated!"})
+    }
+    else{
+        res.json({message:"You're not an admin!"})
+    }
+    
 })
 
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
-    booksData.forEach((el, idx)=>{
-        if (el.id === parseInt(id)){
-            booksData.splice(idx)
-        }
-    });
-    res.json({message: `Book ${id} Deleted!`});
+    let { token } = url.parse(req.url, true).query.token;
+    if (process.env.token == token){
+        booksData.forEach((el, idx)=>{
+            if (el.id === parseInt(id)){
+                booksData.splice(idx)
+            }
+        });
+        res.json({message: `Book ${id} Deleted!`});
+    }
+
+    else{
+        res.json({message: "You're not an admin!"})
+    }
 })
 
 module.exports = router;
